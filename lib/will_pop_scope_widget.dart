aa6fd76fd4e28b0652c0d408c9727528726eb4d8
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-05 15:17:33 +0600

    W will pop score

--- lib/will_pop_scope_widget.dart
+++ lib/will_pop_scope_widget.dart
@@ -12,21 +12,34 @@ class _WillPopScopeWidgetState extends State<WillPopScopeWidget> {
   Widget build(BuildContext context) {
     return Center(
       child: ElevatedButton(
-        onPressed: (){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context){
-            return NextPage();
-          }),)
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return NextPage();
+              },
+            ),
+          );
         },
-        child: child,
+        child: const Text('Next Page'),
       ),
     );
   }
 }
 
 class NextPage extends StatelessWidget {
-  NextPage({Key? key}):super(key: key);
+  NextPage({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context){
-    return;
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        body: Center(),
+        backgroundColor: Colors.blue,
+        appBar: AppBar(title: Text('Next Page')),
+      ),
+    );
   }
 }
