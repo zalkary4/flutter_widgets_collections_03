fc5b6b370848995a25b7366c9d188f509566b07c
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-17 21:00:11 +0600

    W  semantik block

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets_collections_main_3/transform_widget.dart';
 import 'package:flutter_widgets_collections_main_3/widgets/block_semantik_widget034.dart';
+import 'package:flutter_widgets_collections_main_3/widgets/button_navigat_bar_widget035.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
       //   useMaterial3: true,
       // ),
       home: Scaffold(
-        body: BlockSemantikWidget(),
+        body: ButtonNavigatBarWidget035(),
       ),
     );
   }
---
+++ lib/widgets/button_navigat_bar_widget035.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ButtonNavigatBarWidget035 extends StatefulWidget {
+  const ButtonNavigatBarWidget035({Key? key}) : super(key: key);
+
+  @override
+  _ButtonNavigatBarWidget035State createState() =>
+      _ButtonNavigatBarWidget035State();
+}
+
+class _ButtonNavigatBarWidget035State extends State<ButtonNavigatBarWidget035> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
