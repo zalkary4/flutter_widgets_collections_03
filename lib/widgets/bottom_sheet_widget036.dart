4e0b348c0de8d0ab688516bcda49c730abf850dc
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-18 11:47:10 +0600

    W botom sheet

--- lib/widgets/bottom_sheet_widget036.dart
+++ lib/widgets/bottom_sheet_widget036.dart
@@ -10,11 +10,28 @@ class BottomSheetWidget036 extends StatefulWidget {
 class _BottomSheetWidget036State extends State<BottomSheetWidget036> {
   @override
   Widget build(BuildContext context) {
-    return Center( child: ElevatedButton(onPressed: (){
-      showModalBottomSheet(context: context,
-       builder: (BuildContext (context) {
-         return SizedBox()
-       },),)
-    }, child: child),);
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return SizedBox(
+                height: 400,
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Close'),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        child: Text('Modal Bottom Sheet'),
+      ),
+    );
   }
 }
